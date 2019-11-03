610779230bfe4c7e7b5e4e64665938ee73f0377f
Makazzz <makazzzpro@live.ca>
2019-11-03 00:06:58 -0400

    Message when Node.js not installed

--- Start.bat
+++ Start.bat
@@ -1,3 +1,10 @@
 @echo off
-title Adventurer Guild
-node index.js
+title adventurer-guild - discord bot
+IF NOT DEFINED node (
+	color 04
+	echo Node.js is missing please install Node.js before using the bot at https://nodejs.org/
+)
+IF DEFINED node (
+	echo node index.js
+)
+pause
