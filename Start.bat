d147189731eb7eeb336c55e27004cb0185689068
Makazzz <makazzzpro@live.ca>
2019-11-05 23:40:04 -0500

    Upgraded Start.bat
    Added all setup for nodejs and node_modules

--- Start.bat
+++ Start.bat
@@ -1,10 +1,26 @@
 @echo off
 title adventurer-guild - discord bot
-IF NOT DEFINED node (
+IF EXIST "%ProgramFiles%\nodejs\node.exe" goto :MODULES
+IF EXIST "%ProgramFiles(x86)%\nodejs\node.exe" goto :MODULES
+IF EXIST "%ProgramW6432%\nodejs\node.exe" (
+	:MODULES
+	IF EXIST "node_modules" (
+		goto :NODE
+	) ELSE (
+		color 04
+		echo node_modules is missing. Please install them before using the bot with this command
+		echo 'npm install'
+		pause
+		goto :EOF
+	)
+) ELSE (
 	color 04
-	echo Node.js is missing please install Node.js before using the bot at https://nodejs.org/
+	echo Node.js is not installed. Please install Node.js before using the bot at https://nodejs.org/
+	pause
+	goto :EOF
 )
-IF DEFINED node (
-	echo node index.js
-)
+
+:NODE
+color 0B
+node index.js
 pause
